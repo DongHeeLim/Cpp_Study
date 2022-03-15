8e5972936b1649e94493ca942b50dc5e030eb2ba
CoolDong <n_cooldong@naver.com>
2022-03-15 23:57:25 +0900

    .

--- Unit15.4/Unit15.4/sample5.cpp
+++ Unit15.4/Unit15.4/sample5.cpp
@@ -6,7 +6,27 @@ using namespace std;
 
 int main()
 {
+	int num;
+	vector<int> vt;
+	cout << "몇개의 정수 데이터를 입력하시겠습니까?\n";
+	cin >> num;
 
+	for (int i = 0; i < num; i++)
+	{
+		int data;
+		cout << "정수를 입력하십시오. \n";
+		cin >> data;
+		vt.push_back(data); // data를  백터 끝에 추가
+	}
+
+	cout << "표시합니다.\n";
+	vector<int>::iterator it = vt.begin();	// 앞 데이터 
+	while (it != vt.end()) {				// 끝 데이터
+		cout << *it;
+		cout << "-";
+		it++;
+	}
+	cout << "\n";
 
 	return 0;
 }
